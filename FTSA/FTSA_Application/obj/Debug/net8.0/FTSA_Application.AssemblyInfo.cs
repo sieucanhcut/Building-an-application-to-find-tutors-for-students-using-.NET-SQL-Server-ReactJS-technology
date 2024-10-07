07b2d1db696c379c804844364f53c75b81d5f265
Nhan <nhanptde160251@fpt.edu.vn>
2024-10-07 21:43:45 +0900

    TutorRepos

--- FTSA/FTSA_Application/obj/Debug/net8.0/FTSA_Application.AssemblyInfo.cs
+++ FTSA/FTSA_Application/obj/Debug/net8.0/FTSA_Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FTSA_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb195abb192dc909abca995134ac3746eaf34d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a851352454992eee3f51b7ce91f722bfa25ac7f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FTSA_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FTSA_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
